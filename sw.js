f73688cd7d4f950eb6f108fc00db8b35a2840080
Martin Bangho <martin.bangho.ml@gmail.com>
2025-09-17 10:10:43 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+// Service Worker for caching static assets
+const CACHE_NAME = 'bangho-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/assets/css/main.min.css',
+    '/assets/css/bootstrap.min.css',
+    '/assets/css/responsive.css',
+    '/assets/js/main.min.js',
+    '/assets/js/jquery.min.js',
+    '/assets/img/hero/me.webp',
+    '/assets/img/logo.ico'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            // Return cached version or fetch from network
+            return response || fetch(event.request);
+        })
+    );
+});
